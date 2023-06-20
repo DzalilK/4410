88b30cbb1f4a1c26bdf039eda96f686eb13377ed
DzalilK <135230129+DzalilK@users.noreply.github.com>
2023-06-20 08:07:23 +0500

    first_6

--- Program.cs
+++ Program.cs
@@ -1,4 +1,10 @@
-﻿Console.WriteLine("Enter N: ");
+﻿// Напешите программу,которая на вход принимает число и выдает,является 
+//ли число четным 
+
+
+
+
+Console.WriteLine("Enter N: ");
 int N = Convert.ToInt32(Console.ReadLine());
 
 if(N % 2 == 0)
