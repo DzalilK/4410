f56673e3c9bf2c3817490e9a873afdb3beeafd2a
DzalilK <135230129+DzalilK@users.noreply.github.com>
2023-06-20 07:47:20 +0500

    first6

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Enter N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+if(N % 2 == 0)
+{
+    Console.WriteLine($"N == 0");
+}
+else
+{
+    Console.WriteLine($"N == %");
+}
+
